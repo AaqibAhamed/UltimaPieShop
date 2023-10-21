28ff2b4382b9b45ca7ec061b1b65b1dc52c31b57
Ahamed Aaqib <aaqib@edsmart.com>
2023-10-21 13:15:32 +0530

    Add project files.

---
+++ UltimaPieShop/Controllers/PieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UltimaPieShop.Models;
+
+namespace UltimaPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+
+            return View(_pieRepository.AllPies);
+        
+        }
+
+    }
+}
---
+++ UltimaPieShop/Program.cs
@@ -0,0 +1,19 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+
+app.MapDefaultControllerRoute();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run();
