09829aea1683466aacfa875759a6e77df4f9f856
Ahamed Aaqib <aaqib@edsmart.com>
2023-10-23 11:22:04 +0530

    Register services MockCategoryRepository and  MockPieRepository

--- UltimaPieShop/Program.cs
+++ UltimaPieShop/Program.cs
@@ -1,7 +1,12 @@
 
+using UltimaPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ICategoryRepository,MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository,MockPieRepository>();
 
 var app = builder.Build();
 
