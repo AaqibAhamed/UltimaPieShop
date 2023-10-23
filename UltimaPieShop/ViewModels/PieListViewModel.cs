ed4ea2d4c1c0e7e1f99926d2c93b35e2c286b853
Ahamed Aaqib <aaqib@edsmart.com>
2023-10-23 11:11:23 +0530

    Add PieListViewModel

---
+++ UltimaPieShop/ViewModels/PieListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using UltimaPieShop.Models;
+
+namespace UltimaPieShop.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; }
+
+        public string? CurrentCategory { get; }
+
+        public PieListViewModel(IEnumerable<Pie> pies, string? currentCategory)
+        {
+            Pies = pies;
+            CurrentCategory = currentCategory;
+        }
+
+    }
+}
