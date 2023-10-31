815d06f08bfe3e1c6ad00921fe565bb35527912a
Ahamed Aaqib <aaqib@edsmart.com>
2023-10-31 10:11:23 +0530

    Register services

--- UltimaPieShop/Program.cs
+++ UltimaPieShop/Program.cs
@@ -1,12 +1,27 @@
 
+using Microsoft.EntityFrameworkCore;
 using UltimaPieShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddScoped<ICategoryRepository,MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository,MockPieRepository>();
+builder.Services.AddDbContext<UltimaPieShopDbContext>(
+    option =>
+    {
+        option.UseSqlServer(
+        builder.Configuration["ConnectionStrings:UltimaPieShopDbContextConnection"]);
+    });
+
+//builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+//builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(serviceProvider => ShoppingCart.GetCard(serviceProvider));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
@@ -17,8 +32,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 
+app.UseSession();
+
 app.MapDefaultControllerRoute();
 
 //app.MapGet("/", () => "Hello World!");
+
+DataSeeder.Seed(app); // seed data to db for intialization db
 
 app.Run();
