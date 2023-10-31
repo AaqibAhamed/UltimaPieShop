945c880baf92df9fda04e102fbb57ac8bd737ce8
Ahamed Aaqib <aaqib@edsmart.com>
2023-10-31 10:08:53 +0530

    Add HomeController & View

---
+++ UltimaPieShop/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UltimaPieShop.Models;
+using UltimaPieShop.ViewModels;
+
+namespace UltimaPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+             _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            var piesOfTheWeek = _pieRepository.PiesOfTheWeek;
+
+            HomeViewModel homeViewModel = new HomeViewModel(piesOfTheWeek);
+
+            return View(homeViewModel);
+        }
+    }
+}
