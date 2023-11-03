91b24f08ddfc5b243bc79c310c9de05247fb905f
Ahamed Aaqib <aaqib@edsmart.com>
2023-11-03 07:12:16 +0530

    Add PieController and EmailTagHelper Unit Tests

---
+++ UltimaPieShopTest/Controller/PieControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimaPieShop.Controllers;
+using UltimaPieShop.ViewModels;
+using UltimaPieShopTest.Mocks;
+
+namespace UltimaPieShopTest.Controller
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyString_ReturnAllPies()
+        {
+            // Arrange
+            var pioRepo = RepositoryMocks.GetPieRepository();
+
+            var categoryRepo = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController (pioRepo.Object, categoryRepo.Object);
+
+            // Act
+
+            var result = pieController.List("");
+
+            // Assert
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
---
+++ UltimaPieShopTest/TagHelper/EmailTagHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimaPieShop.TagHelpers;
+
+namespace UltimaPieShopTest.TagHelper
+{
+    public class EmailTagHelperTests
+    {
+        [Fact]
+        public void Generates_Email_Link()
+        {
+            EmailTagHelper emailTagHelper = new EmailTagHelper() { Address = "test@ultimapieshop.com", Content = "Email" }; ;
+
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), string.Empty);
+
+            var content = new Mock<TagHelperContent>();
+
+            var tagHelperOutput = new TagHelperOutput("a",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(content.Object));
+
+            // Act
+            emailTagHelper.Process(tagHelperContext, tagHelperOutput);
+
+            Assert.Equal("Email", tagHelperOutput.Content.GetContent());
+            Assert.Equal("a", tagHelperOutput.TagName);
+            Assert.Equal("mailto:test@ultimapieshop.com", tagHelperOutput.Attributes[0].Value);
+        }
+    }
+}
