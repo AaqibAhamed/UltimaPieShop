53f09b83c1a939ceb1f64a44a5ece088ab642197
Ahamed Aaqib <aaqib@edsmart.com>
2023-11-05 07:14:38 +0530

    Add SearchPies logic

--- UltimaPieShop/Models/PieRepository.cs
+++ UltimaPieShop/Models/PieRepository.cs
@@ -34,7 +34,7 @@ namespace UltimaPieShop.Models
 
         public IEnumerable<Pie> SearchPies(string searchQuery)
         {
-            throw new NotImplementedException();
+            return _ultimaPieShopDbContext.Pies.Where(p => p.Name.Contains(searchQuery));
         }
     }
 }
